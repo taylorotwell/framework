7643965d35794fd5ee6e731e6633f3f43939ce38
Mior Muhammad Zaki <crynobone@gmail.com>
2024-08-22 07:15:57 +0800

    Fixes concurrency tests.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Integration/Concurrency/ConcurrencyTest.php
+++ tests/Integration/Concurrency/ConcurrencyTest.php
@@ -2,19 +2,33 @@
 
 namespace Illuminate\Tests\Integration\Console;
 
-use Illuminate\Support\Facades\Concurrency;
 use Orchestra\Testbench\TestCase;
 
 class ConcurrencyTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        $this->defineCacheRoutes(<<<PHP
+<?php
+use Illuminate\Support\Facades\Concurrency;
+
+Route::any('/concurrency', function () {
+    return Concurrency::run([
+        fn () => 1 + 1,
+        fn () => 2 + 2,
+    ]);
+});
+PHP);
+
+        parent::setUp();
+    }
+
     public function testWorkCanBeDistributed()
     {
-        $this->markTestSkipped('Todo...');
+        $response = $this->get('concurrency')
+            ->assertOk();
 
-        [$first, $second] = Concurrency::run([
-            fn () => 1 + 1,
-            fn () => 2 + 2,
-        ]);
+        [$first, $second] = $response->original;
 
         $this->assertEquals(2, $first);
         $this->assertEquals(4, $second);
